fd170b8159c585bbde54eb96aaa24fe9399d2fb3
Kristentf <kristentf2@gmail.com>
2025-08-28 20:56:49 -0300

    rota 'cadastro' feita
    - rota envia 'nome' e 'idade'
    - se vier algum vazio, retorna status 400 (não está funcionando muito bem)

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const cors = require("cors")
 const app = express();
 
-app.get("/",(req, res) => {
-    res.send("Rota principal")
-})
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
     
-app.post("enviar",(req, res)=>{
-   req.body()
-   console.log("informações recebidas") 
+app.post("/cadastro",(req, res)=>{
+   const {nome, idade} = req.body;
+   console.log("informações na rota de cadastro recebidas!") 
+
+if (!nome||!idade){
+    return res.status(400).send("ERRO: preencher campos obrigatórios!")
+}
+res.send(`${nome}, ${idade} anos enviado com sucesso!`)
 })
 
 app.listen (3000, () =>{
