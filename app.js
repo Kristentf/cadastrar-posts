37df3270b593cb06870b0b437adbd6cb41a97ed4
Kristentf <kristentf2@gmail.com>
2025-08-31 15:08:27 -0300

    Mudança para handlebars
    - O back-end não envia mais o arquivo HTML diretamente para o usuário usando res.sendFile. Agora faz uso do handlebars para carregar o HTML, o que deixa o backend mais flexível e facilita a manutenção das páginas.
    - Conectei o DB nomes-test ao Back-end

--- app.js
+++ app.js
@@ -1,24 +1,37 @@
 const express = require("express");
-const cors = require("cors")
 const app = express();
+const exphbs = require("express-handlebars")
+const {Sequelize} = require('sequelize')
+const bodyparser = require('body-parser');
+const { FORCE } = require("sequelize/lib/index-hints");
 
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}))
 
-// Mandar dados pro back
-app.get("/cadastro",(req, res)=>{
-    res.sendFile(__dirname+"/public/cadastro.html")
-    console.log("Rota padrão acessada")
-})
+// Config
+    // Template engine
+    app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars')
+    app.use(express.urlencoded({extended:true}))
+    app.use(express.json())
+    // 
+    
+    
+    // Conexão com o banco de dados  
+    sequelize = new Sequelize('nomes_test','root', '1234',{
+        host: 'localhost',
+        dialect: 'mysql',
+    });
 
-// Receber dados do front
-app.post("/cadastro",(req, res)=>{
-   const {nome, idade} = req.body;
-   console.log("Dados enviados") 
-res.send(`${nome}, ${idade} anos enviado com sucesso!`)
-})
+// Rotas
+    // Rota GET
+    app.get('/',(req,res)=>{
+        res.render("formularios");
+    });
+
+    app.post('/enviar',(req,res)=>{
+        res.send(`Título: ${req.body.titulo} Conteúdo: ${req.body.conteudo}`)
+        console('Dados enviados com sucesso!!')
+    })
 
 app.listen (3000, () =>{
-    console.log("Servidor rodando na porta 3000")
-});
+    console.log("Servidor rodando na porta 3000");
+}); 
